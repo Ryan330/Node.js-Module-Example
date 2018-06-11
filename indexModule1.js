418b563d4a033b94da4a8060aac924a58ee18b8e
“[Ryan <[ryanmiller12@bellsouth.net]>
2018-06-11 10:24:47 -0400

    Node.js Module Example

---
+++ indexModule1.js
@@ -0,0 +1,5 @@
+//Import Data via nodeModule.js
+var colorsData = require("./nodeModule1.js");
+
+//Log Data from nodeModule.js
+console.log(colorsData);
---
+++ nodeModule1.js
@@ -0,0 +1,12 @@
+//Data
+var colors = {
+    firstColor: "Red",
+    secondColor: "Green",
+    thirdColor: "Blue",
+
+    returnData: function () {return "RGB Colors"}
+}
+
+
+//Export Data
+module.exports = colors;
